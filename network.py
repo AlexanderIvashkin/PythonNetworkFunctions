13c0f8d44700cf590afaa9bdba82b15cf915c03a
AlexanderIvashkin <alexander.ivashkin@icloud.com>
2018-12-03 22:11:07 +0100

    Added check IsNumber for each octet

--- network.py
+++ network.py
@@ -1,8 +1,20 @@
+def IsNumber(num):
+    try:
+        float(num)
+    except ValueError:
+        return False
+    return True
+
 def IsIPv4Address(ipAdd):
     """Validate an IPv4 address"""
     octets = ipAdd.split(".")
     if len(octets) != 4:
         return False
+    for octet in octets:
+        if not IsNumber(octet):
+            return False
+
+
     return True
 
 if IsIPv4Address(input("Gimme you address! ")):
