fbe9f4880a68d210cc5b35bbf9a248648c7c586a
AlexanderIvashkin <alexander.ivashkin@icloud.com>
2018-12-03 22:05:30 +0100

    Added check for number of octets

--- network.py
+++ network.py
@@ -1,7 +1,12 @@
 def IsIPv4Address(ipAdd):
     """Validate an IPv4 address"""
     octets = ipAdd.split(".")
-    print (octets)
+    if len(octets) != 4:
+        return False
+    return True
 
-IsIPv4Address(input("Gimme you address! "))
+if IsIPv4Address(input("Gimme you address! ")):
+    print ("TRUEЪ")
+else:
+    print ("NOT TRUEЪ")
 
